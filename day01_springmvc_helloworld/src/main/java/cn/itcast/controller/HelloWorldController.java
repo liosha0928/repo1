e5c5af23e5cdb1eb26704461413c3f69a0394f1f
itcast <bos@itcast.cn>
2019-03-14 14:33:46 +0800

    提交

---
+++ day01_springmvc_helloworld/src/main/java/cn/itcast/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package cn.itcast.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String sayHello(){
+        System.out.println("hello....");
+        return "success";
+    }
+}
---
+++ day01_springmvc_helloworld/src/main/webapp/WEB-INF/pages/success.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>跳转成功的页面</title>
+</head>
+<body>
+
+    <h1 style="color: greenyellow">Hello, SpringMVC......</h1>
+
+</body>
+</html>
